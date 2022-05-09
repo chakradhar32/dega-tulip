0233a3250cb3f1074d5362d4615c28ec8aaaefe8
Mourya Prasanth <mouryaprasanthp@gmail.com>
2022-05-09 20:19:57 +0530

    add tulip card to storyCard elements

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -22,9 +22,99 @@ const StoryCard = ({
   storyData,
   cardStyle = 'basic',
   excerpt = false,
+  type = 'article',
   imageSize = { width: 'full', height: 40 },
 }) => (
   <>
+    {cardStyle === 'tulip' && (
+      <div
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          py: (theme) => `${theme.space.spacing6}`,
+          borderRadius: (theme) => `${theme.borderRadius.default}`,
+          // boxShadow: (theme) => `${theme.boxShadow.default}`,
+          backgroundColor: (theme) => `${theme.colors.background.default}`,
+          color: (theme) => `${theme.colors.text.default}`,
+          '& a:hover': {
+            backgroundColor: (theme) => `${theme.colors.background.hover}`,
+            color: (theme) => `${theme.colors.text.hover}`,
+            textDecoration: 'underline',
+          },
+        }}
+      >
+        <div
+          sx={{
+            display: 'flex',
+          }}
+        >
+          <div
+            className="tulip"
+            sx={{ display: 'flex', flexDirection: 'column', width: 'full', maxWidth: 'full' }}
+          >
+            <div sx={{ maxWidth: '100%', width: '100%', display: 'flex', overflow: 'hidden' }}>
+              <div
+                sx={{
+                  paddingBottom: '56.24999999%',
+                  overflow: 'hidden',
+                  position: 'relative',
+                  width: '100%',
+                }}
+              >
+                <div sx={{ position: 'absolute', width: '100%', height: ' 100%' }}>
+                  <Link passHref href={`/${storyData.slug}`}>
+                    <a
+                      sx={{
+                        zIndex: 20,
+                        display: 'block',
+                        width: '100%',
+                        height: '100%',
+                        position: 'absolute',
+                        textDecoration: 'none',
+                        touchAction: 'manipulation',
+                      }}
+                    >
+                      <img
+                        sx={{ height: '100%', objectFit: 'cover' }}
+                        src={storyData.medium?.url.proxy}
+                      />
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div
+              key={storyData.id}
+              sx={{ borderTop: '1px solid #d9d9d9', display: 'block', py: '1rem' }}
+            >
+              <Link key={storyData.id} href={`category/${storyData.categories[0].slug}`} passHref>
+                <a
+                  sx={{
+                    fontSize: '0.75rem',
+                    textTransform: 'uppercase',
+                    color: '#df1c22',
+                    display: 'block',
+                  }}
+                >
+                  {storyData.categories.length > 0 ? storyData.categories[0].name : ''}
+                </a>
+              </Link>
+              <Link key={storyData.id} href={`/${storyData.slug}`} passHref>
+                <a sx={{ display: 'block' }}>
+                  <h3 sx={{ fontSize: '1rem' }}>{storyData.title}</h3>
+                </a>
+              </Link>
+              <Link key={storyData.id} href={`/author/${storyData.users[0].slug}`} passHref>
+                <a sx={{ fontSize: '0.75rem', textTransform: 'uppercase' }}>
+                  {storyData.users[0].display_name}
+                </a>
+              </Link>
+              <p sx={{ fontSize: '0.675rem' }}>{parseDate(storyData.published_date)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
     {cardStyle === 'basic' && (
       <article
         className={`${cardStyle}`}
